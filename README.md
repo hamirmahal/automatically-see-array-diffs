14e4f3fd264a65c3990028cd7b522bd688d871d0
Hamir Mahal <hamirmahal@gmail.com>
2023-02-05 12:46:26 -0800

    feat: tool that automatically shows diffs

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a tool that should let you automatically see the differerences between two arrays in your terminal.
+
+# Instructions
+
+1. Run `npm start`.
+2. Copy and paste the actual and expected arrays into `actual.txt` and `expected.txt`, respectively.
+3. You should see changes in your terminal automatically after saving either file.
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+
+const parseFile = (pathToFile) => {
+  try {
+    return JSON.parse(fs.readFileSync(pathToFile));
+  } catch (error) {
+    console.error(`Failed to parse file "${pathToFile}"`);
+    throw Error(error);
+  }
+};
+
+const pathToActual = './src/actual.txt';
+const pathToExpected = './src/expected.txt';
+const actual = parseFile(pathToActual);
+const expected = parseFile(pathToExpected);
+if (!Array.isArray(actual) || !Array.isArray(expected)) {
+  console.error(
+    actual,
+    'or',
+    expected,
+    'is not an array, but they both should be.'
+  );
+  throw Error();
+}
+actual.sort();
+expected.sort();
+const pathToSortedActual = './tmp/sortedActual.js';
+const pathToSortedExpected = './tmp/sortedExpected.js';
+fs.writeFileSync(pathToSortedActual, JSON.stringify(actual, null, 2));
+fs.writeFileSync(pathToSortedExpected, JSON.stringify(expected, null, 2));
+
+const command = `git diff --color --no-index ${pathToSortedExpected} ${pathToSortedActual}`;
+try {
+  execSync(command, { encoding: 'utf-8' });
+  console.info('Both inputs match.');
+} catch (e) {
+  console.info('You need to make the following changes:');
+  console.info(e.stdout);
+}
+console.log(new Date().toLocaleTimeString());
